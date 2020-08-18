55ff9dc3464926278bd5c16dc3f42b895aef7cd3
Nick <deckards2505@gmail.com>
2020-08-18 22:16:32 +0600

    Init repo

---
+++ gulp/styles.js
@@ -0,0 +1,43 @@
+const gulp = require('gulp');
+const plumber = require('gulp-plumber');
+const scss = require('gulp-sass');
+const cleanCSS = require('gulp-clean-css');
+const sourcemaps = require('gulp-sourcemaps');
+const autoprefixer = require('gulp-autoprefixer');
+const argv = require('yargs').argv;
+const gulpif = require('gulp-if');
+const concat = require('gulp-concat');
+const rename = require('gulp-rename');
+
+
+// Работаем со стилями
+
+module.exports = function styles() {
+    gulp.src([
+        'node_modules/normalize.css/normalize.css',
+    ])
+        .pipe(concat('libs.css'))
+        .pipe(cleanCSS({
+            compatibility: 'ie8'
+        }))
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(gulp.dest('app/css'));
+    return gulp.src('app/scss/style.scss')
+        .pipe(plumber())
+        .pipe(gulpif(!argv.prod, sourcemaps.init()))
+        .pipe(scss())
+        .pipe(autoprefixer({
+            overrideBrowserslist:  [ "last 10 version" ],
+            cascade: false
+        }))
+        .pipe(gulpif(argv.prod, cleanCSS({
+            debug: true,
+            compatibility: '*'
+        }, details => {
+            console.log(`${details.name}: Original size:${details.stats.originalSize} - Minified size: ${details.stats.minifiedSize}`)
+        })))
+        .pipe(gulpif(!argv.prod, sourcemaps.write()))
+        .pipe(gulp.dest('dist/css/'))
+};
